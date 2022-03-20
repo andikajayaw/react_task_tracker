6037ddb9bee80867de9fdf419313969fe77c816a
andikajayaw <andikajayawiguna@gmail.com>
2022-03-20 21:41:13 +0800

    update: add show button function for Add Task, change Title and Color for button with props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -44,10 +46,15 @@ function App() {
     setTasks(tasks.map((task) => task.id == id ? { ...task, reminder: !task.reminder} : task))
   }
 
+  const toggleAddTask = () => {
+    console.log(showAddTask);
+    setShowAddTask(!showAddTask);
+  }
+
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={toggleAddTask} showAdd={showAddTask} />
+      { showAddTask && <AddTask onAdd={addTask} />}
       {
         tasks.length > 0 ? 
         (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) 
